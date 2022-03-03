d6ed7dc36bb5c8cf9efa19a012004bd29e59666a
Harsha <harsha@shipx.in>
2022-03-03 07:47:19 +0000

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,10 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Allow connections to local server.
   config.hosts.clear
   
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  #config.assets.js_compressor = Uglifier.new(harmony: true)
 end
