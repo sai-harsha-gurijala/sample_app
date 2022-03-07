dc38919d58952bf21c33d3f15c2a8ba9e47f8858
Harsha <harsha@shipx.in>
2022-03-07 16:20:22 +0000

    updated

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -16,8 +16,8 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_template 'password_resets/new'
     # Valid email
-    post password_resets_path,
-         params: { password_reset: { email: @user.email } }
+   # post password_resets_path,
+    #     params: { password_reset: { email: @user.email } }
     assert_not_equal @user.reset_digest, @user.reload.reset_digest
     assert_equal 1, ActionMailer::Base.deliveries.size
     assert_not flash.empty?
